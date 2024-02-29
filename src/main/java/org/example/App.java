bd84336dc61d9c911ce80b905814d1e9dab4dbd8
Jason <chensong823@gmail.com>
2024-02-28 19:38:00 -0500

    code was made

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,9 +8,27 @@ public class App
 {
 
     public static String fun1(int i){
-      return "";
+        if (i % 3 == 0 && i % 5 == 0) {
+            return "FizzBuzz";
+        }
+        else if(i % 3 == 0){
+            return "Fizz";
+        }
+        else if(i % 5 == 0){
+            return "Buzz";
+        }
+        else {
+            return Integer.toString(i);
+        }
     }
         public static void main(String[] args) {
+            String[] str = new String[100];
+            for(int i = 1; i <= 100; i++){
+                str[i-1] = fun1(i);
+            }
 
+            for (String s : str) {
+                System.out.print(s + ",");
+            }
         }
 }
