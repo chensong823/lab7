556162aefd06cc0fa988561e200ebf710f5bf9ce
Jason <chensong823@gmail.com>
2024-02-28 20:14:10 -0500

    CheckBuzz passed

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,6 +14,9 @@ public class App
         else if(i % 3 == 0){
             return "Fizz";
         }
+        else if(i % 5 == 0){
+            return "Buzz";
+        }
 
         return " ";
     }
