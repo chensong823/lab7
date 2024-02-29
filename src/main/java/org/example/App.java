27e47b7490cf4ea2578e5fd334ff0f137d6b4e07
Jason <chensong823@gmail.com>
2024-02-28 20:09:44 -0500

    CheckFizzBuzz

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,28 +7,10 @@ package org.example;
 public class App 
 {
 
-    public static String fun1(int i){
-        if (i % 3 == 0 && i % 5 == 0) {
-            return "FizzBuzz";
-        }
-        else if(i % 3 == 0){
-            return "Fizz";
-        }
-        else if(i % 5 == 0){
-            return "Buzz";
-        }
-        else {
-            return Integer.toString(i);
-        }
-    }
-        public static void main(String[] args) {
-            String[] str = new String[100];
-            for(int i = 1; i <= 100; i++){
-                str[i-1] = fun1(i);
-            }
+    public static String fun1(int i) {
 
-            for (String s : str) {
-                System.out.print(s + ",");
-            }
-        }
+
+        return " ";
+    }
+
 }
